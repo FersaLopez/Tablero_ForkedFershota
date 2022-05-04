06478d001108c4897df6f0ab75a3fdb9d0073fc4
Fersa <321172974@alumno.enp.unam.mx>
2022-05-04 11:45:38 -0500

    Maquetado HTML en el php

--- dynamics/index.php
+++ dynamics/index.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título bonito</title>
+</head>
+<body>
 <?php
-
 $size = $_POST["size"];
 
 echo "<table border='2'>";
@@ -17,7 +25,8 @@ for ($i = 0; $i < $size; $i++) {
 
     echo "</tr>";
 }
-
 echo "</table>";
-
 ?>
+</body>
+</html>
+
